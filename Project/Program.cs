c86d6ea0e55f679041523124d4f2bb7df329eceb
Shrimpey304 <kimo.edelbroek@gmail.com>
2024-03-20 11:24:02 +0100

    implemented sessions

--- Project/Program.cs
+++ Project/Program.cs
@@ -5,7 +5,12 @@ public class Program{
     public static void Main(){
 
         const string fileName = "DataStorage/CinemaRoom1.json";
-        DisplayRoom.SelectSeating(fileName);
+        const string fileNameSesh = "DataStorage/Sessions.json";
+        // MovieSession.MakeSession(DateTime.Now, DateTime.Now.AddHours(1));
+        List<MovieSession> sessions = SessionsJsonUtils.ReadFromJson(fileNameSesh);
+        MovieSession sesh = sessions[0];
+
+        DisplayRoom.SelectSeating(fileName, sesh);
         
     }
 
