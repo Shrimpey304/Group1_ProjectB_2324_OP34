a8678b17a0dc0367d52c1778005c38867ef83b1e
Shrimpey <kimo.edelbroek@gmail.com>
2024-03-26 09:38:16 +0100

    testerietus

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,14 +7,15 @@ public class Program{
 
 		const string fileName = "DataStorage/CinemaRoom1.json";
 		const string fileNameSesh = "DataStorage/Sessions.json";
+		
+		List<MovieSession> sessions = SessionsJsonUtils.ReadFromJson(fileNameSesh);
+		MovieSession sesh = sessions[0];
+
+		DisplayRoom.SelectSeating(fileName, sesh);
 		// MovieSession.MakeSession(DateTime.Now, DateTime.Now.AddHours(1));
-		
-		
-		// List<MovieSession> sessions = SessionsJsonUtils.ReadFromJson(fileNameSesh);
-		// MovieSession sesh = sessions[0];
 
 		// DisplayRoom.SelectSeating(fileName, sesh);
-		Menu.Start(); // Start the application menu
+		// Menu.Start(); // Start the application menu
 		
 		// Movie m1 = new Movie("title 1", 13, "Not Horror", "Very not-scary");
 		// Movie m2 = new Movie("Scary radio", 18, "Very scared", "OH NO");
